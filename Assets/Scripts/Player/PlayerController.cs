6d6501bcd0ac854ed4f601698296b9ebd9d51310
seratonin0073 <mizko2000y@gmail.com>
2023-05-07 23:53:08 +0300

    FixedFinalCommit

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
